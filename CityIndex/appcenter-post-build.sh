414e4ecef78ebafe7bf1d3baf81a682b6eff014c
Natalia <natalya.zhenchak@gmail.com>
2019-04-05 11:55:25 +0200

    no device

--- CityIndex/appcenter-post-build.sh
+++ CityIndex/appcenter-post-build.sh
@@ -105,7 +105,7 @@ env
 
     echo "Appcenter"
 
-    appcenter test run uitest --app "natalya.zhenchak-gmail.com/Xamarin.iOS" --token "132d433074220a6d4fd3583b81a38bbe4b0c8466" --devices "4c82ff37" --app-path $IPAFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --dsym-dir $DSYMDirectory
+    appcenter test run uitest --app "natalya.zhenchak-gmail.com/Xamarin.iOS" --token "132d433074220a6d4fd3583b81a38bbe4b0c8466" --app-path $IPAFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --dsym-dir $DSYMDirectory
     echo "After appcenter"
 
 
